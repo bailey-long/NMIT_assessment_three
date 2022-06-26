da636e35d12c7bbd32f94da4198f56a8c72fbbbb
Loopypew <fivebailey2@gmail.com>
2022-06-26 21:37:28 +1200

    assessment three files

---
+++ Task_One/Task_One/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numOne = 0;
+            double numTwo = 0;
+            Console.WriteLine("Calculator\n-------------");
+            //Get user input for first number, inside of while loop so user does not attempt to divide by zero
+            while (numOne == 0)
+            {
+                Console.WriteLine("Type first number: ");
+                numOne = Convert.ToDouble(Console.ReadLine());
+            }
+            //Get user input for second number, inside of while loop so user does not attempt to divide by zero
+            while (numTwo == 0)
+            {
+                Console.WriteLine("Type second number: ");
+                numTwo = Convert.ToDouble(Console.ReadLine());
+            }
+            //Display operator choices to user 
+            Console.WriteLine("Math operators:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("\tp - Power of");
+            Console.WriteLine("\tr - remainder of");
+            Console.Write("Select Operator ");
+            //Take user input, convert to upper case so that the switch can take lower and upper case
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            switch (Console.ReadLine().ToUpper())
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            {
+                default:
+                    Console.WriteLine("Invalid Choice, try again");
+                    break;
+                //Add
+                case "A":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Your Result: {numOne} + {numTwo} = " + (numOne + numTwo));
+                    //reset console colour after printing result
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    break;
+                //Subtract
+                case "S":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"Your Result: {numOne} - {numTwo} = " + (numOne - numTwo));
+                    //reset console colour after printing result
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                //Multiply
+                case "M":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Your Result: {numOne} * {numTwo} = " + (numOne * numTwo));
+                    //reset console colour after printing result
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                //Divide
+                case "D":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"Your Result: {numOne} / {numTwo} = " + (numOne / numTwo));
+                    //reset console colour after printing result
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                //Power
+                case "P":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Your Result: {numOne} to the power of {numTwo} = " + Math.Pow(numOne, numTwo));
+                    //reset console colour after printing result
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                //Remainder
+                case "R":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Your Result: remainder of {numOne} / {numTwo} = " + (numOne % numTwo));
+                    //reset console colour after printing result
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+    }
+}
---
+++ Task_Two/Task_Two/Program.cs
@@ -0,0 +1,30 @@
+﻿//new array that largest numbers will go into
+int[] newArray = { 0,0,0 };
+//array sorting funciton
+void arrSort(int[][] arr)
+{
+    //loop through array
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            //int to hold value of array value
+            int largest = arr[i][0];
+            for (int j = 0; j < arr.GetLength(0); j++)
+            {
+                //if array index is bigger than "largest" reassign largest value to the index
+                if (arr[i][j] > largest)
+                {
+                    largest = arr[i][j];
+                    //set array index to largest value
+                    newArray[i] = largest;
+                }
+            }
+        }
+    //join newArray values into string seperarated by comma
+    Console.WriteLine(String.Join(",", newArray.Cast<int>()));
+}
+//setup of jagged array
+int[][] array = new int[2][];
+array[0] = new int[] { 4, 2, 7, 1 };
+array[1] = new int[] { 20, 70, 40, 90 };
+array[2] = new int[] { 1, 2, 0 };
+arrSort(array);
